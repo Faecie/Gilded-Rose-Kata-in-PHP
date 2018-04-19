d114de2c5714af918edf8ab38d2e605b5f78164c
Alejandro Estringana Ruiz <alejandro.ruiz@theiconic.com.au>
2018-04-19 23:51:32 +1000

    Variance04 ready

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,22 +4,5 @@ namespace App;
 
 class GildedRose
 {
-    public $name;
 
-    public $quality;
-
-    public $sellIn;
-
-    public function __construct($name, $quality, $sellIn)
-    {
-
-    }
-
-    public static function of($name, $quality, $sellIn) {
-        return new static($name, $quality, $sellIn);
-    }
-
-    public function tick()
-    {
-    }
 }
